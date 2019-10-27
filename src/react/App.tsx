405c477ce3df71db0e2b98e9a948eb04b8093da1
zverev <alexander.zverev@gmail.com>
2019-10-27 16:03:10 +0300

    Create and use Button component

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import { style } from "typestyle";
 import * as csstips from "csstips";
 import InfluxDBClient from "../influxdb"
+import Button from "./Button";
 
 const client = new InfluxDBClient("http://94.103.95.29:8086/write?db=main", false);
 
 function App() {
   return (
     <div className={style(csstips.fillParent, csstips.centerCenter)}>
-      <div
-        className={style({
-          background: "#0000ff",
-          padding: "12px 24px",
-          color: "white",
-          cursor: "pointer",
-        })}
+      <Button
+        title="Add Point"
         onClick={() => {
           client.point('key',{value:1},{tag:'tag_name'});
           client.send();
         }}
-      >
-        Add Point
-      </div>
+      />
     </div>
   );
 }
---
+++ src/react/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { style } from "typestyle";
+
+interface IButtonProps {
+  title: string;
+  onClick: () => void;
+}
+function Button(props: IButtonProps) {
+  return (
+    <div
+      className={style({
+        background: "#0000ff",
+        padding: "12px 24px",
+        color: "white",
+        cursor: "pointer",
+      })}
+      onClick={props.onClick}
+    >
+      {props.title}
+    </div>
+  )
+}
+
+export default Button;
